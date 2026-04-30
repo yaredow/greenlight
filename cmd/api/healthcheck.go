2fa8052f58ff000b4d742ee1dc50c0c57291e161
yaredow <yaredyilma11@gmail.com>
2026-04-30 13:46:25 +0300

    feat: finish a basic error handling implimentation

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -13,7 +13,7 @@ func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Reques
 
 	err := app.writeJSON(w, http.StatusOK, envelope{"data": data}, nil)
 	if err != nil {
-		app.logger.Print(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
+		return
 	}
 }
