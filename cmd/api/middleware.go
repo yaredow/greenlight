68bb1d459c3e1afe03efe8f2b9be8ddfb25d46f9
yardow <yaredyilma11@gmail.com>
2026-05-24 11:43:20 +0300

    feat: finish configuring simple and preflight CORS

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -17,13 +17,9 @@ import (
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
+		w.Header().Set("Access-Control-Max-Age", "60")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization, *")
 
 		next.ServeHTTP(w, r)
 	})
